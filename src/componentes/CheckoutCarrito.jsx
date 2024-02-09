ec244ce005efa762b03b98c9eac24b29f0c7a2f0
Gonza <gnzx89@gmail.com>
2024-02-09 15:07:23 -0300

    ...

--- src/componentes/CheckoutCarrito.jsx
+++ src/componentes/CheckoutCarrito.jsx
@@ -2,16 +2,13 @@ import React , { useState }from 'react';
 import '../estilos/CheckoutCarrito.css';
 
 function CheckoutCarrito (props) {
-     
-    
+         
     return (
-        <div className={props.carritoClickeado ? 'checkout-contenedor visible' : 'checkout-contenedor'}>
-            <div className={props.carritoClickeado ? 'checkout-carrito-contenedor visible' : 'checkout-carrito-contenedor'}>
+        <div className={ props.carritoClickeado ? 'checkout-contenedor visible' : 'checkout-contenedor'}>
+            <div className='checkout-carrito-contenedor visible'>
              <span className="botondecierre">X</span>   
             </div>  
         </div>
-          
-
     )
 }
 
